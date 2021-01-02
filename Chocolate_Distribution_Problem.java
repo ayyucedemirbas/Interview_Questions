ee8e2f335fcfca67fd6e4719a6bcd0b59a5ad015
Ayyuce Demirbas <ayyucedemirbas@windowslive.com>
2021-01-02 19:09:13 +0300

    Create Chocolate_Distribution_Problem.java

---
+++ Chocolate_Distribution_Problem.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+/*Given an array A of positive integers of size N, where each value represents number of chocolates in a packet. Each packet can have variable number of chocolates. There are M students, the task is to distribute chocolate packets such that :
+1. Each student gets one packet.
+2. The difference between the number of chocolates given to the students having packet with maximum chocolates and student having packet with minimum chocolates is minimum.
+
+Input:
+The first line of input contains an integer T, denoting the number of test cases. Then T test cases follow. Each test case consists of three lines. The first line of each test case contains an integer N denoting the number of packets. Then next line contains N space separated values of the array A denoting the values of each packet. The third line of each test case contains an integer m denoting the no of students.
+
+Output:
+For each test case in a new line print the minimum difference.
+
+Constraints:
+1 <= T <= 100
+1 <=N<= 107
+1 <= Ai <= 1018
+1 <= M <= N
+
+Example:
+Input:
+2
+8
+3 4 1 9 56 7 9 12
+5
+7
+7 3 2 4 9 12 56
+3
+
+Output:
+6
+2
+
+Explanation:
+Testcase 1: The minimum difference between maximum chocolates and minimum chocolates is 9-3=6
+
+*/
+class Chocolate_Distribution_Problem {
+    
+   static void bubbleSort(int arr[]) { 
+        int n = arr.length; 
+        for (int i = 0; i < n-1; i++) 
+            for (int j = 0; j < n-i-1; j++) 
+                if (arr[j] > arr[j+1]) 
+                { 
+                    // swap arr[j+1] and arr[j] 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j+1]; 
+                    arr[j+1] = temp; 
+                }
+                
+   }
+   
+   
+     
+	public static void main (String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        int test = s.nextInt();
+        for(int i=0; i<test; i++){
+            int size = s.nextInt();
+            int[] packet = new int[size];
+            
+            for(int j =0; j< size; j++){
+                packet[j] = s.nextInt();
+            }
+            int student = s.nextInt();
+            
+            
+            bubbleSort(packet);
+            int min_diff = packet[size-1];
+            for (int j = 0; j + student - 1 < size; j++){
+            int diff = packet[j+student-1] - packet[j];
+            if (diff < min_diff){
+                min_diff = diff;
+            }
+        }
+            
+            System.out.println(min_diff);
+            
+        }
+        
+       
+        
+	}
+}
