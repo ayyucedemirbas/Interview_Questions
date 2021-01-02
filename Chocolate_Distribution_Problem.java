f529e4fdf7a00716efabb9a283bc189bd06a7f93
Ayyuce Demirbas <ayyucedemirbas@windowslive.com>
2021-01-02 19:16:47 +0300

    The program takes less time with updated changes

--- Chocolate_Distribution_Problem.java
+++ Chocolate_Distribution_Problem.java
@@ -54,6 +54,74 @@ class Chocolate_Distribution_Problem {
                 }
                 
    }
+	
+	
+   static void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+ 
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+ 
+        /* Merge the temp arrays */
+ 
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+ 
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+ 
+    // Main function that sorts arr[l..r] using
+    // merge()
+    static void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m = (l + r) / 2;
+ 
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
    
    
      
@@ -71,7 +139,8 @@ class Chocolate_Distribution_Problem {
             int student = s.nextInt();
             
             
-            bubbleSort(packet);
+            //bubbleSort(packet);
+	     sort(packet, 0, size-1);
             int min_diff = packet[size-1];
             for (int j = 0; j + student - 1 < size; j++){
             int diff = packet[j+student-1] - packet[j];
