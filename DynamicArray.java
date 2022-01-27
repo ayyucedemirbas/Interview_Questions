6bf457bf6ac9826c4670e77d9a312f7aa9622560
Ayyuce Demirbas <a.ayyuced@gmail.com>
2022-01-27 14:20:18 +0300

    Add files via upload

---
+++ DynamicArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class DynamicArray{
+    public static void main(String[] args) {
+        List<Integer> gasPrices = new ArrayList<>();
+
+        gasPrices.add(346);
+        gasPrices.add(360);
+        gasPrices.add(354);
+        System.out.println(gasPrices);
+    }
+
+}
